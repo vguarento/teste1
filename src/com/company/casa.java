6fd60623797c247693f1072ca503fde7e22a053b
guarento <valdecir.guarento2011@gmail.com>
2022-03-25 14:39:25 -0300

    teste inicial

---
+++ src/com/company/casa.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class casa {
+    public static void main(String[] args) {
+        String endereco = "Rua Timburi N 30";
+
+        System.out.println("Minha casa fica na " + endereco);
+    }
+}
